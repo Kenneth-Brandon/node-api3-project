a231dbfe1d26d974b45109a3056e64af1daafcfc
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-05-13 17:38:48 -0700

    server.js running on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code away!
+const express = require('express');
+
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
+server.get('/', (request, response) => {
+  response.send('<h1>User and Post API</h1>');
+});
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`\r\n Server running on http://localhost:${PORT}\r\n`);
+});
